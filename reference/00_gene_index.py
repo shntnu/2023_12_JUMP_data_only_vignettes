71174f2f16ff4efe2637ea1784c504bf633641fd
Alán F. Muñoz <afer.mg@gmail.com>
2025-03-21 10:34:42 -0400

    fix(gene_names): update header

--- reference/00_gene_index.py
+++ reference/00_gene_index.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env jupyter
 # ---
 # title: Available genes
 # jupyter:
@@ -11,19 +12,20 @@
 #     display_name: Python 3
 #     language: python
 #     name: python3
-# format:
-#   html:
-#     code-fold: true
+# ---
+
 # %% [markdown]
 # Is my Gene in JUMP or other associated tables?
 
 
 # %%
+# | code-fold: true
 import polars as pl
 import pooch
 from itables import show
 
 # %%
+# | code-fold: true
 logger = pooch.get_logger()
 logger.setLevel("WARNING")
 df = pl.read_csv(
@@ -33,10 +35,5 @@ df = pl.read_csv(
     )
 )
 show(df, maxBytes=0)
-df = pl.read_csv(
-    pooch.retrieve(
-        "https://zenodo.org/api/records/15059555/files/table.csv/content",
-        known_hash="a082225c08a09f95fd6578dc7d2114aea772ff728f75314fc1350647e0ac949f",
-    )
-)
+df = pl.read_csv(pooch.retrieve("https://zenodo.org/api/records/15059555/files/table.csv/content", known_hash  = "a082225c08a09f95fd6578dc7d2114aea772ff728f75314fc1350647e0ac949f"))
 show(df, maxBytes=0)
