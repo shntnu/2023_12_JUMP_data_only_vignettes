96d38a956cc713f91bdfc57f4272a99de3eee91a
Alán F. Muñoz <afer.mg@gmail.com>
2025-03-21 10:21:06 -0400

    deploy: add manual gene index quarto conversion

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,6 +2,7 @@
 # Script that converts python scripts into notebooks, runs and publishes them to a website using Quarto and
 # generates a notebook in another branch that mimics the original one, but adds a cell containing '!pip install jump_deps'
 jupytext --sync scripts/*.py &&
+jupytext --to qmd ../reference/00_gene_index.py &&
 git config --global user.email quarto-github-actions-publish@example.com &&
 git config --global user.name  Quarto_GHA_Runner &&
 export QUARTO_PYTHON=$(which python) &&
